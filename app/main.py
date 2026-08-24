2ac3da5ffd1aff760cbbb8e9a205df8d2a5cd6bb
Oleksii Hrabovyi <oleksii.hrabovyi.kb.2024@lpnu.ua>
2026-08-24 19:14:44 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
 def get_human_age(cat_age: int, dog_age: int) -> list:
+    if type(cat_age) is not int or type(dog_age) is not int:
+        raise TypeError("Age must be an integer")
+
     def calculate_human_years(animal_age: int,
                               years_per_human_year: int) -> int:
         if animal_age < 15:
@@ -10,5 +13,4 @@ def get_human_age(cat_age: int, dog_age: int) -> list:
 
     cat_human_age = calculate_human_years(cat_age, 4)
     dog_human_age = calculate_human_years(dog_age, 5)
-
     return [cat_human_age, dog_human_age]
--- app/test_main.py
+++ app/test_main.py
@@ -6,6 +6,10 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_age, dog_age, expected",
     [
+        (-1, -1, [0, 0]),
+        (-15, -24, [0, 0]),
+        (-100, -50, [0, 0]),
+
         (0, 0, [0, 0]),
         (14, 14, [0, 0]),
         (15, 15, [1, 1]),
@@ -21,3 +25,20 @@ from app.main import get_human_age
 )
 def test_get_human_age(cat_age: int, dog_age: int, expected: list) -> None:
     assert get_human_age(cat_age, dog_age) == expected
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age",
+    [
+        ("15", 15),
+        (15, "15"),
+        (15.5, 15),
+        (15, 15.5),
+        (None, 15),
+        (15, None),
+        ([], {}),
+    ]
+)
+def test_get_human_age_wrong_types(cat_age: any, dog_age: any) -> None:
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
